fd76f8291058bbdb8632698674050f5167a27531
钟乘永 <zhongcy93@gmail.com>
2019-08-03 12:19:44 +0800

    Modify code

--- sword_offer/chapter2_base.h
+++ sword_offer/chapter2_base.h
@@ -4,9 +4,52 @@
 
 #ifndef CPPLIB_CHAPTER2_BASE_H
 #define CPPLIB_CHAPTER2_BASE_H
-namespace sword{
+
+#include <iostream>
+#include <string>
+
+namespace sword {
     // 一个空的对象，不包含任何成员和函数，求sizeof
-    class Empty{};
+    class Empty {
+    };
+
+    //如果包含析构函数，则实例的sizeof结果为8，因为有一个指向虚函数表的指针，64位机器上是8字节
+    class EmptyWithVirtualMethod {
+    public:
+        virtual ~EmptyWithVirtualMethod() = default;
+    };
+
+    // 赋值构造函数必须是深拷贝
+    class CMyString {
+    public:
+        explicit CMyString(char *str = nullptr) : m_pData(str) {}
+
+        /**
+         * Not safe.
+         * @param str
+         * @return
+         */
+//        CMyString &operator=(const CMyString &str) {
+//            if(this == &str){
+//                return *this;
+//            }
+//            delete[]m_pData;
+//            m_pData = nullptr;
+//            m_pData = new char[strlen(str.m_pData) + 1];
+//            strcpy(m_pData, str.m_pData);
+//            return *this;
+//        }
+        CMyString&operator=(const CMyString &str){
+
+        }
+        ~CMyString(){
+            std::cout << "DST invoked" << std::endl;
+            delete []m_pData;
+        }
+
+        char *m_pData;
+    };
+
 }
 
 
--- sword_offer/sword_offer_test.cpp
+++ sword_offer/sword_offer_test.cpp
@@ -9,5 +9,17 @@ using namespace std;
 TEST(SWORDOFFER_CHAPTER2, SIZEOF){
     Empty empty;
     EXPECT_EQ(sizeof(empty), 1);
+    EmptyWithVirtualMethod emptyWithVirtualMethod;
+    EXPECT_EQ(8,sizeof(emptyWithVirtualMethod));
 
+}
+
+TEST(SWORDOFFER_CHAPTER2, ASSIGN){
+    char* v = "abc";
+    CMyString str;
+    str.m_pData = v;
+    CMyString str2;
+    str2 = str;
+    str2.m_pData = "def";
+    cout << str2.m_pData << endl;
 }
